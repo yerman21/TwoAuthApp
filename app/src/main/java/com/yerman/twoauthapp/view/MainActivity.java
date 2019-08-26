e2c0a46262aebf94ef6fd526010d5ca1a2b8ecf1
yerman21 <danyaguirre@upeu.edu.pe>
2019-08-26 11:53:42 -0500

    avanzando algo

--- app/src/main/java/com/yerman/twoauthapp/view/MainActivity.java
+++ app/src/main/java/com/yerman/twoauthapp/view/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
